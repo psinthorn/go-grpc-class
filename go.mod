f628b5d5a8269b34c758bd4f9958da2d62adc90d
Sinthorn P <psinthorn@gmail.com>
2021-01-03 12:18:14 +0700

    Go mod init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/psinthorn/go-grpc-class
+
+go 1.15
---
+++ greet/server/server.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+type server struct{}
+
+func main() {
+	fmt.Println("Hello gRPC")
+	s := grpc.NewServer()
+	greetpb.RegisterGreetServiceServer(s, &server{})
+	listener, err := net.Listen("tcp", "0.0.0.0:50051")
+	if err != nil {
+		log.Fatalf("Failed to listen on : %v", err)
+	}
+
+	if err := s.Serve(listener); err != nil {
+		log.Fatalf("Failed to listen on : %v", err)
+	}
+}
